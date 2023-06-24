e1cb2fe606dc3d7a4a9cdb76e40528cb3840b089
Yarkarthik <135239471+Yarkarthik@users.noreply.github.com>
2023-06-24 14:42:33 +0530

    commit

--- user-card-grid/src/UserCardGrid.js
+++ user-card-grid/src/UserCardGrid.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 
 const Navbar = styled.nav`
@@ -7,15 +7,6 @@ const Navbar = styled.nav`
   align-items: center;
   padding: 1rem;
   background-color: #f2f2f2;
-`;
-
-const Button = styled.button`
-  margin-top: 1rem;
-  padding: 0.5rem 1rem;
-  background-color: #007bff;
-  color: #fff;
-  border: none;
-  cursor: pointer;
 `;
 
 const Loader = styled.div`
@@ -41,16 +32,32 @@ const ListItem = styled.li`
   margin-bottom: 1rem;
 `;
 
+const PageNumbers = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 2rem;
+`;
+
+const PageNumber = styled.span`
+  cursor: pointer;
+  margin: 0 0.5rem;
+  color: ${(props) => (props.active ? "#007bff" : "inherit")};
+  font-weight: ${(props) => (props.active ? "bold" : "normal")};
+`;
+
 const UserCardGrid = () => {
   const [users, setUsers] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
 
-  const getUsers = async () => {
+  const getUsers = async (page) => {
     setIsLoading(true);
     try {
-      const response = await fetch("https://reqres.in/api/users?page=1");
+      const response = await fetch(`https://reqres.in/api/users?page=${page}`);
       const data = await response.json();
       setUsers(data.data);
+      setTotalPages(data.total_pages);
     } catch (error) {
       console.error("Error fetching user data:", error);
     } finally {
@@ -58,25 +65,72 @@ const UserCardGrid = () => {
     }
   };
 
+  useEffect(() => {
+    getUsers(currentPage);
+  }, [currentPage]);
+
+  const handlePageClick = (page) => {
+    setCurrentPage(page);
+  };
+
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage((prevPage) => prevPage + 1);
+    }
+  };
+
+  const handlePreviousPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage((prevPage) => prevPage - 1);
+    }
+  };
+
   return (
     <>
       <Navbar>
-        <h1>USER CARD</h1>
-        <Button onClick={getUsers}>Get Users</Button>
+        <h1>Hello ReqRes users!</h1>
       </Navbar>
 
       {isLoading ? (
         <Loader>Loading...</Loader>
       ) : (
-        <List>
-          {users.map((user) => (
-            <ListItem key={user.id}>
-              <img src={user.avatar} alt={user.first_name} />
-              <h3>{`${user.first_name} ${user.last_name}`}</h3>
-              <p>{user.email}</p>
-            </ListItem>
-          ))}
-        </List>
+        <>
+          <List>
+            {users.map((user) => (
+              <ListItem key={user.id}>
+                <img src={user.avatar} alt={user.first_name} />
+                <h3>{`${user.first_name} ${user.last_name}`}</h3>
+                <p>{user.email}</p>
+              </ListItem>
+            ))}
+          </List>
+
+          <PageNumbers>
+            <PageNumber
+              onClick={handlePreviousPage}
+              disabled={currentPage === 1}
+            >
+              &lt; Previous
+            </PageNumber>
+            {Array.from({ length: totalPages }, (_, index) => index + 1).map(
+              (page) => (
+                <PageNumber
+                  key={page}
+                  onClick={handlePageClick.bind(null, page)}
+                  active={page === currentPage}
+                >
+                  {page}
+                </PageNumber>
+              )
+            )}
+            <PageNumber
+              onClick={handleNextPage}
+              disabled={currentPage === totalPages}
+            >
+              Next &gt;
+            </PageNumber>
+          </PageNumbers>
+        </>
       )}
     </>
   );
